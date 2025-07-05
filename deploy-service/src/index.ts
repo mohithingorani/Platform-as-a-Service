d889d3cc80c4fc608599f3f474631439bba1eb0f
mohithingorani <mohithingorani2003@gmail.com>
2025-07-06 01:13:13 +0530

    Created an infinitely running that pulls values from redis queue and initialized deployment service

---
+++ deploy-service/src/index.ts
@@ -0,0 +1,13 @@
+import {createClient} from "redis"
+
+const subscriber = createClient();
+subscriber.connect();
+
+async function main() {
+    while(true){
+        const response = await subscriber.brPop('build-queue', 0);
+        console.log(response);
+    }
+}
+
+main();

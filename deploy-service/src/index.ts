b15314ea858e251ee828de35b4877696d24efa01
mohithingorani <mohithingorani2003@gmail.com>
2025-07-06 01:46:02 +0530

    fixed file sepertors

--- deploy-service/src/index.ts
+++ deploy-service/src/index.ts
@@ -7,7 +7,13 @@ async function main() {
     while(true){
         const response = await subscriber.brPop('build-queue', 0);
         console.log(response);
+
+        const id = response?.element;
+        await downloadFromS3()
+        
+
     }
+
 }
 
 main();

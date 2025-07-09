6f1a2072a83fdd60f60f100ac8494569f2237666
mohithingorani <mohithingorani2003@gmail.com>
2025-07-09 21:11:27 +0530

    request handler working

--- request-handler/src/index.ts
+++ request-handler/src/index.ts
@@ -1,6 +1,3 @@
-import express from "express";
-import { S3 } from "aws-sdk";
-import * as dotenv from "dotenv";
 dotenv.config();
 
 const s3 = new S3({
@@ -9,24 +6,41 @@ const s3 = new S3({
   endpoint: process.env.endpoint,
 });
 
-
 const app = express();
-
 app.get("*", async (req, res) => {
-    const host = req.hostname;
+  const host = req.hostname;
+  const id = host.split(".")[0];
+  const filePath = req.path === "/" ? "/index.html" : req.path;
+  const key = `dist/${id}${filePath}`;
 
-    const id = host.split(".")[0];
-    const filePath = req.path === "/" ? "/index.html" : req.path;
+  console.log("key=", key);
 
-    const contents = await s3.getObject({
+  try {
+    const contents = await s3
+      .getObject({
         Bucket: "paas",
-        Key: `dist/${id}${filePath}`
-    }).promise();
-    
-    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+        Key: key,
+      })
+      .promise();
+
+    const type = filePath.endsWith("html")
+      ? "text/html"
+      : filePath.endsWith("css")
+      ? "text/css"
+      : "application/javascript";
+
     res.set("Content-Type", type);
-
     res.send(contents.Body);
-})
+  } catch (err: any) {
+    if (err.code === "NoSuchKey") {
+      res.status(404).send("File not found");
+    } else {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
+  }
+});
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log("Server listening on port 3001");
+});

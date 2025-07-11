0f50d90d29386edd9a071cc33232dd8a09949062
mohithingorani <mohithingorani2003@gmail.com>
2025-07-12 00:54:37 +0530

    checking on server

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
+import { useState } from "react";
 import Card from "./components/Card";
 import LogsCard from "./components/LogsCard";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
-
 export default function Home() {
   return (
     <main className=" flex justify-center items-center min-h-screen w-full bg-gray-50 dark:bg-gray-900 transition-colors duration-500 p-4">
@@ -12,7 +12,6 @@ export default function Home() {
       </div>
 
       <Card />
-      <LogsCard id={"12"}/>
     </main>
   );
 }

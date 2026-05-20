2a119d3293e6e5f48511a95ea8c7525fb3356270
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-05-21 00:06:43 +0530

    login button

--- frontend/src/app/components/LoginButton.tsx
+++ frontend/src/app/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import {useRouter} from "next/navigation"
 
 export default function LoginButton(){
    const router = useRouter();
-   return <button onClick(()=>router.push("/login) className="opacity-50 text-xs md:text-sm hover:opacity-100">
+   return <button onClick={()=>router.push("/login")} className="opacity-50 text-xs md:text-sm hover:opacity-100">
                 Login
             </button>
 }
